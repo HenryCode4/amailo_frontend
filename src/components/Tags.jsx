bf30546c0dc8d7c1a2334ba070ca0ea73f93fa1c
henry nnamani <nnamani.henry@sevenup.org>
2024-02-24 19:14:06 +0100

    updated

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,21 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
+ // className={'container-tags'}
+      // style={{
+      //   justifyContent: forFoodPage ? 'start' : 'center',
+      // }}
 export default function Tags({ tags, forFoodPage }) {
+  console.log(tags);
   return (
     <div
-      className={'container-tags'}
-      style={{
-        justifyContent: forFoodPage ? 'start' : 'center',
-      }}
+     
+      className=' pb-6 px-6 pt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 justify-center items-center '
     >
       {tags.map(tag => (
-        <Link key={tag.name} to={`/tag/${tag.name}`}>
-          {tag.name}
-          {!forFoodPage && `(${tag.count})`}
+        <Link className='m-4 shadow-md rounded-full flex flex-col justify-center items-center' key={tag.name} to={`/tag/${tag.name}`}>
+          <img className='w-[100px] h-[100px] object-cover rounded-full' src={`/icons/${tag.imageUrl}`}  alt="" />
+          
+          <p className='font-semibold text-xl pb-2 text-gray-500'>{tag.name}
+          {!forFoodPage && `(${tag.count})`}</p>
+          
         </Link>
       ))}
     </div>
   );
 }
+
+// const TagCard = ({ tag, forFoodPage }) => {
+//   return (
+//     <div className="bg-white rounded-lg shadow-md p-4 m-2">
+      
+//       <Link className="text-xl font-semibold text-gray-800 hover:text-blue-500" to={`/tag/${tag.name}`}>
+//         {tag.name}
+//       </Link>
+      
+//       {!forFoodPage && <span className="text-sm text-gray-600 ml-1">({tag.count})</span>}
+//     </div>
+//   );
+// };
+
+// const TagList = ({ tags, forFoodPage }) => {
+//   return (
+//     <div className="pt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
+//       {tags.map(tag => (
+//         <TagCard key={tag.name} tag={tag} forFoodPage={forFoodPage} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default TagList;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -37,12 +37,37 @@ const HomePage = () => {
     }, [searchTerm, tag])
   return (
     <>
+    
+    <div className='flex flex-col md:flex-row justify-center gap-x-32 items-center px-2'>
+        <div className='w-[350px] md:w-[500px]'>
+            <h1 className='text-2xl md:text-5xl font-bold tracking-wide pb-3'>Enjoy Your <span className='text-[#FF582E] border-b-4 border-[#FF582E] rounded-xl'>Food</span> </h1>
+            <h3 className='text-2xl md:text-5xl font-bold tracking-wide pb-6'>Without Leaving The House</h3> 
+            <p className='text-[.8rem] text-gray-500 pb-8'>Satisfy your craving without stepping out - indulge in a diverse culinary experience delivered straight to your doorstep, Elevate your home dining with a menu that caters to your taste.</p>
+
+            <div className='flex gap-x-4'>
+                <a href='#order-now' className='bg-[#273238] text-white text-[0.9rem] font-bold px-4 py-2 rounded-full'>Order Now</a>
+            <a href='/orders' className='border-2 border-gray-300 text-gray-500 text-[0.9rem] px-4 py-2 rounded-full font-bold '>Track your order</a>
+            </div>
+            
+        </div>
+        
+        <img className='w-[350px] h-[350px] md:w-[550px] md:h-[550px]' src='/icons/delivery.jpg' alt="" />
+    </div>
+
+    <div className='pt-2' id='order-now'>
     <Search />
+
+    <p className='text-center text-gray-500 text-[1.3rem] font-bold'>Choose your preferred restaurant</p>
     <Tags tags={tags}/>
+
+
     {
         foods.length === 0 && <NotFound linkText={'Reset Search'}/>
     }
-    <Thumbnails foods={foods} />
+    
+        <Thumbnails foods={foods} />
+    </div>
+    
     </>
     
   )
